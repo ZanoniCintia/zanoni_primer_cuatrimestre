d2b63ce0fecbd62ac94dea761bb04fe7e61d8626
zanonicintia <cintu94@hotmail.com>
2019-04-23 18:31:38 -0300

    Avance Matrices

--- array/main.c
+++ array/main.c
@@ -26,7 +26,7 @@ int arrayInicializar(char arrayStrn[][50],int limite)
 }
 int posicionVacia(char arrayStrn[][50],int limite)
 {
-int i;
+    int i;
 
     int retorno;
 
@@ -37,32 +37,57 @@ int i;
             retorno=i;
             break;
        }
-       else
-       continue;
+
     }
     return retorno;
 }
+int nombreDuplicado(char arrayStrn [][50],char* nombre,int* nombreExistente,int limite)
+{
+    int retorno =-1;
+    int i;
+    for(i=0;i<limite;i++)
+    {
+        if(strncmp(arrayStrn[i],nombre,limite)==0)
+        {
+           *nombreExistente=i;
+           retorno =0;
+           break;
+        }
+    }
+    return retorno;
+}
+int altaPersonal(char* msg,char* msgError,int minimo,int maximo,int limite,char array[][],int reintentos)
+{
+    int retorno=-1;
+    int posicionRepetida = -1;
+    char buffer[4096];
 
+    if(msg!=NULL,msgError!=NULL,minimo<maximo,reintentos>=0)
+    {
+        if(posicionVacia(array,limite)!=-1)
+        {
+            getName(msg,msgError,minimo,maximo,reintentos,&buffer);
+            nombreDuplicado(array,buffer,&posicionRepetida,limite);
+            if(posicionRepetida==-1)
+            {
+                strncpy(array[posicionVacia(array,limite)],buffer,maximo);
+                retorno =0;
+            }
+        }
+
+    }
+
+
+}
 int main()
 {
     char auxName[500] = "test";
+    int test=-1;
     char arrayNombres[QTY_EMPLEADOS][50];
     strncpy(arrayNombres[0],"juan0",50);
     strncpy(arrayNombres[1],"juan1",50);
     strncpy(arrayNombres[2],"juan2",50);
 
-    if(getName( "Nombre?: ",
-                "Error",
-                2,
-                5,
-                2,
-                auxName) == 0)
-    {
-        printf("OK: %s",auxName);
-    }
-    else
-    {
-        printf("ER: %s",auxName);
-    }
-    return 0;
+    nombreDuplicado(arrayNombres,"roberto",&test,50);
+    printf("%d", test);
 }
