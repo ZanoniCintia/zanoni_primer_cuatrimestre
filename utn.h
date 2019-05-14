34191f35e8755b1a7f8f2eee9cb35a606c1cce2b
zanonicintia <cintu94@hotmail.com>
2019-05-14 14:27:35 -0300

    utn

--- utn.h
+++ utn.h
@@ -18,7 +18,28 @@ int getName (   char* msg,
 
 
 int isValidName (char* cadena);
-
-
+int isValidChar(char letra, char maximo, char minimo);
+int isValidArrayInt(int numero, int maximo, int minimo);
+int getArrayInt(    char *msg,
+                    char *msgError,
+                    int maximo,
+                    int minimo,
+                    int reintentos,
+                    int *arrayResultado,
+                    int limite);
+int printArrayInt(int *arrayResultado, int limite);
+int doSwap(int *primerValor, int *segundoValor);
+int ordenarArray(char *vector[10][50], int limite);
+int getString(  char *mensaje,
+                char *mensajeError,
+                int minimo,
+                int maximo,
+                int reintentos,
+                char *sString);
+int isValidLengthString(char* buffer, int minimo, int maximo);
+int encontrarNombre(char sArray[][50],char *nombre , int limite);
+int encontrarVacio(char sArray[][50], int limite);
+int inicializar(char sArray[][50], int limite);
+int printArrayArray(char aString[][50], int limite);
 
 
